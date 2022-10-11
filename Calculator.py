bd8a99691812f44be7b08ab44b06cb8c1771eef9
SHIVAM PRAKASH <67830887+SHIVAMP149@users.noreply.github.com>
2022-10-11 09:18:27 +0530

    Calculator

--- Calculator.py
+++ Calculator.py
@@ -1,24 +1,40 @@
 # Asking input from users
 
-x = int(input("ENTER THE FIRST NO : "))
-y = int(input("ENTER THE SECOND NO : "))
+num1 = int(input("ENTER THE FIRST NO : "))
+num2 = int(input("ENTER THE SECOND NO : "))
 
 # Choosing the operater
+
 operator = input("ENTER THE OPERATOR : (+,-,*,/,%,//,**) : ")
+
+# ADDITION
 if operator == "+":
-    print("SUM IS : ",x+y)
+    print("SUM IS : ",num1+num2)
+
+# SUBTRACTION
 elif operator == "-":
-    print("SUBTRACTION IS : ",x-y)
+    print("SUBTRACTION IS : ",num1-num2)
+
+# MULTIPLICATION
 elif operator == "*":
-    print("MULTIPLICATION IS : ",x*y)
+    print("MULTIPLICATION IS : ",num1*num2)
+
+# DIVISION
 elif operator == "/":
-    print("DIVISION IS : ",x/y)
+    print("DIVISION IS : ",num1/num2)
+
+# MODULUS
 elif operator == "%":
-    print("MODULUS IS : ",x%y)
+    print("MODULUS IS : ",num1%num2)
+
+# FLOOR DIVISION
 elif operator == "//":
-    print("FLOOR DIVISION : ",x//y)
+    print("FLOOR DIVISION : ",num1//num2)
+
+# POWER
 elif operator == "**":
-    print("POWER IS : ",x**y)
+    print("POWER IS : ",num1**num2)
+
 else:
     print("INVALID OPERATOR")
 
